2fa6b8585a7c68c6efd4eac250e54849647f1091
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-11-04 14:59:02 -0400

    add initial specs

--- src/cljs/re_frame_notifier/core.cljs
+++ src/cljs/re_frame_notifier/core.cljs
@@ -7,7 +7,23 @@
   "Initial re-frame-notifier state.
 
   All requests are associated into a map."
-  {})
+  {:alerts []
+   :modals []
+   :toasts []})
+
+;; Specs for each type of notification
+(s/def :alert/title string?)
+(s/def ::alerts (s/coll-of (s/keys :req-un [:alert/title])))
+
+(s/def :modal/title string?)
+(s/def ::modals (s/coll-of (s/keys :req-un [:modal/title])))
+
+(s/def :toast/message string?)
+(s/def ::toasts (s/coll-of (s/keys :req-un [:toast/message])))
+
+(s/def ::notifier (s/keys :req-un [::alerts
+                                   ::modals
+                                   ::toasts]))
 
 (defn register-events
   "Registers re-frame-request events and request handler"
