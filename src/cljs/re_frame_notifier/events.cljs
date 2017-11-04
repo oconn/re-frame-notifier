182a1c1d24012718f757d5abd05773322d423fe1
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-11-04 14:59:21 -0400

    add create events

--- src/cljs/re_frame_notifier/events.cljs
+++ src/cljs/re_frame_notifier/events.cljs
@@ -4,6 +4,16 @@
 (defn register-events
   []
   (reg-event-db
-   :notifier/something
-   (fn [db [_ {:keys [name request-time error status]}]]
-     db)))
+   :notifier/create-alert
+   (fn [db [_ [_ alert]]]
+     (update-in db [:notifier :alerts] #(conj % alert))))
+
+  (reg-event-db
+   :notifier/create-modal
+   (fn [db [_ [_ modal]]]
+     (update-in db [:notifier :modals] #(conj % modal))))
+
+  (reg-event-db
+   :notifier/create-toast
+   (fn [db [_ [_ toast]]]
+     (update-in db [:notifier :toasts] #(conj % toast)))))
