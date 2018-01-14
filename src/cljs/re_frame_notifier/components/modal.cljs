67f046ee289c79d782320a7aa313573c8fc897ee
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2018-01-14 13:55:41 -0500

    update modal component

--- src/cljs/re_frame_notifier/components/modal.cljs
+++ src/cljs/re_frame_notifier/components/modal.cljs
@@ -4,14 +4,23 @@
 (defn modal-component
   [{:keys [component
            component-props
-           hide-dismiss]
+           hide-dismiss
+           dismiss-button-class
+           modal-container-class
+           modal-component-container-class
+           modal-underlay-class]
     :or {component-props nil
-         hide-dismiss false}}]
-  [:div.notifier-underlay
-   [:div.notifier-modal
-    [component component-props]
+         hide-dismiss false
+         dismiss-button-class "notifier-dismiss-button"
+         modal-container-class "notifier-modal-container"
+         modal-component-container-class "notifier-modal-component-container"
+         modal-underlay-class "notifier-underlay"}}]
+  [:div {:class modal-underlay-class}
+   [:div {:class modal-container-class}
     (when-not hide-dismiss
       [:button
-       {:class "notifier-dismiss-button"
+       {:class dismiss-button-class
         :on-click (action-and-close identity :modal)}
-       "X"])]])
+       "X"])
+    [:div {:class modal-component-container-class}
+     [component component-props]]]])
